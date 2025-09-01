9431e4e406ac55d9f085f9fbd66e51bee2855d7b
Bethel Amadi <amadibethel@icloud.com>
2025-09-01 11:41:58 +0200

    Add updated files to Project

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.APP_NAME = exports.API_URL = void 0;
+exports.API_URL = 'https://api.example.com/properties';
+exports.APP_NAME = 'ALX Listing App';
+// Add other constants here as needed
